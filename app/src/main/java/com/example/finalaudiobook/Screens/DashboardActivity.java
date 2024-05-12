29477af49b9e1773611c58a72484647e4055acc1
AqsaDilshad36 <dilshadaqsa36@gmail.com>
2024-05-12 22:47:42 +0500

    done

--- app/src/main/java/com/example/finalaudiobook/Screens/DashboardActivity.java
+++ app/src/main/java/com/example/finalaudiobook/Screens/DashboardActivity.java
@@ -1,6 +1,9 @@
 package com.example.finalaudiobook.Screens;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +20,15 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        findViewById(R.id.).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DashboardActivity.this, SeeMoreActivity.class));
+            }
+        });
 
     }
 }
