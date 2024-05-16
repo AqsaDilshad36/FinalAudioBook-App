8dc6a8bb1004a5f1f598feea3bc19a87d465eca9
AqsaDilshad36 <dilshadaqsa36@gmail.com>
2024-05-17 01:50:07 +0500

    login

--- app/src/main/java/com/example/finalaudiobook/Screens/LoginActivity.java
+++ app/src/main/java/com/example/finalaudiobook/Screens/LoginActivity.java
@@ -2,8 +2,13 @@ package com.example.finalaudiobook.Screens;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,12 @@ import com.example.finalaudiobook.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText emailInput, pwdInput;
+    Button loginBtn;
+    CheckBox rememberMe;
+    ProgressBar loader;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +34,13 @@ public class LoginActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_login);
+
+        emailInput = findViewById(R.id.emailInput);
+        pwdInput = findViewById(R.id.pwdInput);
+        rememberMe = findViewById(R.id.rememberMe);
+        loader = findViewById(R.id.loader);
+        loginBtn = findViewById(R.id.loginBtn);
+
         findViewById(R.id.forgetbtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,12 +56,46 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.loginBtn).setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, WelcomeActivity.class));
+                validation();
             }
         });
+
+
     }
+    public boolean emailValidation(){
+        String input = emailInput.getText().toString().trim();
+        if(input.equals("")){
+            emailInput.setError("Email Address is Required!!!");
+            return false;
+        } else if(!Patterns.EMAIL_ADDRESS.matcher(input).matches()){
+            emailInput.setError("Enter Valid Email Address!!!");
+            return false;
+        } else {
+            emailInput.setError(null);
+            return true;
+        }
+    }
+
+    public boolean pwdValidation(){
+        String input = pwdInput.getText().toString().trim();
+        if(input.equals("")){
+            pwdInput.setError("Password is Required!!!");
+            return false;
+        } else if(input.length() < 8){
+            pwdInput.setError("Password at least 8 Characters!!!");
+            return false;
+        } else {
+            pwdInput.setError(null);
+            return true;
+        }
+    }
+
+    public void validation(){
+        boolean emailErr = false, pwdErr = false;
+        emailErr = emailValidation();
+        pwdErr = pwdValidation();
 
     }
--- app/src/main/java/com/example/finalaudiobook/Screens/RegisterActivity.java
+++ app/src/main/java/com/example/finalaudiobook/Screens/RegisterActivity.java
@@ -1,23 +1,42 @@
 package com.example.finalaudiobook.Screens;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Patterns;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.finalaudiobook.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -202,7 +221,98 @@ public class RegisterActivity extends AppCompatActivity {
         pwdErr = pwdValidation();
         cpwdErr = cpwdValidation();
         if((nameErr && emailErr && pwdErr && cpwdErr) == true){
-            loader
+            loader .setVisibility(View.VISIBLE);
+            registerBtn.setVisibility(View.GONE);
+            Dialog dialog=new Dialog(RegisterActivity.this);
+            dialog.setContentView(R.layout.dialog_loading);
+            dialog.getWindow(). setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.setCancelable(false);
+            TextView msg=dialog.findViewById(R.id.msgDialog);
+            msg.setText("Loading...");
+            dialog.show();
+
+//            Declare Firebase Authentication Object
+            FirebaseAuth auth= FirebaseAuth.getInstance();
+
+//            Create User in Firebase Authentication
+            auth.createUserWithEmailAndPassword(emailInput.getText().toString(),pwdInput.getText().toString())
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+//                            Get UID from Firebase Authentication
+                            String userId= auth.getCurrentUser().getUid();
+
+
+                            // Create a SimpleDateFormat object with the desired format.
+                            SimpleDateFormat sdf = new SimpleDateFormat("dd-MMMM-yyyy", Locale.getDefault());
+
+                            // Format the date to a string
+                            String currentDate = sdf.format(new Date());
+
+//                            Create Object By HashMap
+
+                            DatabaseReference myRef= FirebaseDatabase.getInstance().getReference();
+                            HashMap<String,String> obj=new HashMap<String,String>();
+                            obj.put("name",nameInput.getText().toString().trim());
+                            obj.put("Email",emailInput.getText().toString().trim());
+                            obj.put("Image","");
+                            obj.put("Role","User");
+                            obj.put("Created On",currentDate);
+                            obj.put("Status","1");
+
+                            // Upload Data into Realtime Database
+                            myRef.child("User").child(userId).setValue(obj);
+                            dialog.dismiss();
+                            Dialog dialog = new Dialog(RegisterActivity.this);
+                            dialog.setContentView(R.layout.dialog_success);
+                            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                            dialog.setCanceledOnTouchOutside(false);
+                            dialog.setCancelable(false);
+                            TextView msg=dialog.findViewById(R.id.msgDialog);
+                            msg.setText("Account Created Successfully!!!");
+                            dialog.show();
+
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    dialog.dismiss();
+                                    RegisterActivity.super.onBackPressed();
+                                }
+                            },4000);
+
+                        }
+                    })
+
+                              .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    dialog.dismiss();
+                                    Dialog dialog = new Dialog(RegisterActivity.this);
+                                    dialog.setContentView(R.layout.dialog_error);
+                                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                                    dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    dialog.setCanceledOnTouchOutside(false);
+                                    dialog.setCancelable(false);
+                                    TextView msg = dialog.findViewById(R.id.msgDialog);
+                                    msg.setText("Your Email Already Exist!!!");
+                                    dialog.show();
+                                    new Handler().postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            dialog.dismiss();
+                                            loader.setVisibility(View.GONE);
+                                            registerBtn.setVisibility(View.VISIBLE);
+                                        }
+                                    },4000);
+                                }
+                            });
+
+
+
+
 
         }
     }
