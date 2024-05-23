f9f22f3cfd53448d68a7cef1c0ea94783e725253
AqsaDilshad36 <dilshadaqsa36@gmail.com>
2024-05-24 01:23:45 +0500

    go

--- app/src/main/java/com/example/finalaudiobook/Screens/DetailActivity.java
+++ app/src/main/java/com/example/finalaudiobook/Screens/DetailActivity.java
@@ -1,6 +1,9 @@
 package com.example.finalaudiobook.Screens;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,29 @@ public class DetailActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_detail);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        findViewById(R.id.backBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DetailActivity.super.onBackPressed();
+            }
+        });
+
+        findViewById(R.id.readBookBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailActivity.this, ReadBookActivity.class));
+            }
+        });
+
+        findViewById(R.id.playAudioBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailActivity.this, PlayerActivity.class));
+            }
+        });
 
 
     }
